94ed01b5c6a54ff12a5ba64c01bacbcdc77c4e7c
rajasapr <rajasapr@gmail.com>
2025-10-17 20:44:52 -0700

    feat: add new cli flag --dry-run

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ Task:
 def should_consider(filename: str) -> bool:
     important_exts = (".py", ".ts", ".js", ".md", ".yml", ".yaml", ".toml", ".json")
     return filename.endswith(important_exts) or filename.startswith(("docs/", "app/", "src/"))
+def this_is_test():
+    return "Test sucess"
 
 @app.get("/health")
 def health():
